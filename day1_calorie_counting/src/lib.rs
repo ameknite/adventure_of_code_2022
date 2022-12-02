4ecb891b0b0d914b662a1eb954e381c19cfe58e9
atinyame <104745335+atinyame@users.noreply.github.com>
2022-12-01 20:49:24 -0600

    --- Day 1: Calorie Counting ---

---
+++ day1_calorie_counting/src/lib.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+pub fn data() -> String {
+    fs::read_to_string("input.txt").unwrap()
+}
+
+pub fn part1(data: &str) -> u32 {
+    data.split("\n\n")
+        .map(|elf| elf.lines().map(|food| food.parse::<u32>().unwrap()).sum())
+        .max()
+        .unwrap()
+}
+
+pub fn part2(data: &str) -> u32 {
+    let mut v = data
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|food| food.parse::<u32>().unwrap()).sum())
+        .collect::<Vec<u32>>();
+    v.sort_unstable();
+    v.iter().rev().take(3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DATA: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn test_part1() {
+        let result = part1(DATA);
+        assert_eq!(result, 24000);
+    }
+    #[test]
+    fn test_part2() {
+        let result = part2(DATA);
+        assert_eq!(result, 45000);
+    }
+}
---
+++ day1_calorie_counting/src/main.rs
@@ -0,0 +1,6 @@
+use day1_calorie_counting::*;
+
+fn main() {
+    println!("Part 1: {}", part1(&data()));
+    println!("Part 2: {}", part2(&data()));
+}
